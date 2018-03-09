7cbda312039dd11f131692c3a7c38b9bae803cbe
RREYLES <rreyles@virtela.net>
2018-03-09 21:44:25 +0800

    Fields for emplyee time record

--- src/main/java/net/virtela/TimeRecord/model/EmployeeTimeRecord.java
+++ src/main/java/net/virtela/TimeRecord/model/EmployeeTimeRecord.java
@@ -1,5 +1,53 @@
 package net.virtela.TimeRecord.model;
 
+import java.util.Date;
+
 public class EmployeeTimeRecord {
+
+	private Long employeeCode;
+	private String emloyeeName;
+	private Date date;
+	private Date timeIn;
+	private Date timeOut;
+
+	public Long getEmployeeCode() {
+		return employeeCode;
+	}
+
+	public void setEmployeeCode(Long employeeCode) {
+		this.employeeCode = employeeCode;
+	}
+
+	public String getEmloyeeName() {
+		return emloyeeName;
+	}
+
+	public void setEmloyeeName(String emloyeeName) {
+		this.emloyeeName = emloyeeName;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Date getTimeIn() {
+		return timeIn;
+	}
+
+	public void setTimeIn(Date timeIn) {
+		this.timeIn = timeIn;
+	}
+
+	public Date getTimeOut() {
+		return timeOut;
+	}
+
+	public void setTimeOut(Date timeOut) {
+		this.timeOut = timeOut;
+	}
 
 }

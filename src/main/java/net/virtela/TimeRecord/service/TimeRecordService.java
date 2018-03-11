8ca24dd4aac5b150333d28d92c8d5dd03f140e3d
RREYLES <rreyles@virtela.net>
2018-03-11 17:16:43 +0800

    removed unused imports

--- src/main/java/net/virtela/TimeRecord/service/TimeRecordService.java
+++ src/main/java/net/virtela/TimeRecord/service/TimeRecordService.java
@@ -6,14 +6,11 @@ import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 import org.apache.commons.lang3.StringUtils;
-import org.assertj.core.util.DateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import net.virtela.TimeRecord.dao.TimeRecordDao;

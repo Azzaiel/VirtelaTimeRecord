74c93a1cdfa9cb3db262777043fab8ea3ee66d57
RREYLES <rreyles@virtela.net>
2018-03-12 02:06:28 +0800

    Object to String correction

--- src/main/java/net/virtela/TimeRecord/utils/Constants.java
+++ src/main/java/net/virtela/TimeRecord/utils/Constants.java
@@ -4,15 +4,15 @@ public class Constants {
 	
 	public static final String SPACE = " ";
 	
-	public static final Object PATH_SEPARATOR = "/";
+	public static final String PATH_SEPARATOR = "/";
 
 	public static final String SLASH = "/";
 
 	public static final String EMPTY_STRING = "";
 
-	public static final Object DOT = ".";
+	public static final String DOT = ".";
 
-	public static final Object FILE_EXTENSION_XLSX = "xlsx";
+	public static final String FILE_EXTENSION_XLSX = "xlsx";
 	
 
 }

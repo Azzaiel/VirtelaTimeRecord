0e5c74fa2726f4dbbc3a46102ae4aec7382ff21c
Richard Reyles <azzaielda@gmail.com>
2018-03-12 20:38:29 +0800

    ANsi isntall option

--- src/main/java/net/virtela/TimeRecord/TimeRecordApplication.java
+++ src/main/java/net/virtela/TimeRecord/TimeRecordApplication.java
@@ -1,5 +1,6 @@
 package net.virtela.TimeRecord;
 
+import org.fusesource.jansi.AnsiConsole;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class TimeRecordApplication {
 
 	public static void main(String[] args) {
+		AnsiConsole.systemInstall(); //Install Ansi to console.
 		SpringApplication.run(TimeRecordApplication.class, args);
 	}
 	
